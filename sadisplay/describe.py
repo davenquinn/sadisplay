41b418e7bf822887a8edafcf072577aa404a0c36
Daven Quinn <code@davenquinn.com>
2023-12-20 14:31:33 -0500

    Updates for sqlite

--- sadisplay/describe.py
+++ sadisplay/describe.py
@@ -17,15 +17,18 @@ try:
 except ImportError:
     from sqlalchemy.sql.expression import Label
 
-SQLALCHEMY_VERSION = tuple(map(int, sqlalchemy.__version__.split('.')))
+SQLALCHEMY_VERSION = tuple(map(int, sqlalchemy.__version__.split(".")))
 
 
-def describe(items,
-             show_methods=True,
-             show_properties=True,
-             show_indexes=True,
-             show_simple_indexes=True,
-             show_columns_of_indexes=True):
+def describe(
+    items,
+    show_methods=True,
+    show_properties=True,
+    show_indexes=True,
+    show_simple_indexes=True,
+    show_columns_of_indexes=True,
+    default_schema=None,
+):
     """Detecting attributes, inherits and relations
 
     :param items: list of objects to describe
@@ -88,18 +91,19 @@ def describe(items,
 
     def column_role(column):
         if column.primary_key:
-            return 'pk'
+            return "pk"
         elif column.foreign_keys:
-            return 'fk'
+            return "fk"
 
     def column_compare(a, b):
         prefix = {
-            'pk': '0',
-            'fk': '1',
+            "pk": "0",
+            "fk": "1",
         }
         return locale.strcoll(
-            prefix.get(a[2], '2') + a[1],
-            prefix.get(b[2], '2') + b[1], )
+            prefix.get(a[2], "2") + a[1],
+            prefix.get(b[2], "2") + b[1],
+        )
 
     def get_indexes(entity):
         indexes = []
@@ -111,12 +115,14 @@ def describe(items,
             if not show_simple_indexes and len(index.columns) <= 1:
                 continue
 
-            indexes.append({
-                'name':
-                index.name,
-                'cols':
-                get_columns_of_index(index) if show_columns_of_indexes else [],
-            })
+            indexes.append(
+                {
+                    "name": index.name,
+                    "cols": get_columns_of_index(index)
+                    if show_columns_of_indexes
+                    else [],
+                }
+            )
 
         return indexes
 
@@ -125,6 +131,7 @@ def describe(items,
 
     class EntryItem(object):
         """Class adaptor for mapped classes and tables"""
+
         name = None
         methods = []
         columns = []
@@ -134,7 +141,6 @@ def describe(items,
         bases = tuple()
 
         def __init__(self, mapper=None, table=None):
-
             if mapper is not None:
                 self.name = mapper.class_.__name__
                 self.columns = mapper.columns
@@ -160,7 +166,7 @@ def describe(items,
                 pass
 
         def __repr__(self):
-            return '<{s.__class__.__name__} {s.name}>'.format(s=self)
+            return "<{s.__class__.__name__} {s.name}>".format(s=self)
 
         def __eq__(self, other):
             if other.inherits or self.inherits:
@@ -187,66 +193,73 @@ def describe(items,
             entries.append(entity)
 
     for entry in entries:
-
         result_item = {
-            'name': entry.name,
-            'schema': entry.schema,
-            'cols': [(column_type(col), name, column_role(col), )
-                     for name, col in entry.columns.items()
-                     if not isinstance(col, Label)],
-            'indexes': [],
-            'props': [],
-            'methods': [],
+            "name": entry.name,
+            "schema": getattr(entry, "schema", default_schema),
+            "cols": [
+                (
+                    column_type(col),
+                    name,
+                    column_role(col),
+                )
+                for name, col in entry.columns.items()
+                if not isinstance(col, Label)
+            ],
+            "indexes": [],
+            "props": [],
+            "methods": [],
         }
 
         # sort columns by role and name
-        result_item['cols'].sort(
-            key=cmp_to_key(column_compare), )
+        result_item["cols"].sort(
+            key=cmp_to_key(column_compare),
+        )
 
         if show_methods and entry.methods:
-
             if entry.inherits:
                 base_methods = entry.inherits.class_.__dict__.keys()
             else:
                 # Create the DummyClass subclass of mapper bases
                 # for detecting mapper own methods
-                suffix = '%s' % str(uuid.uuid4()).replace('-', '_')
+                suffix = "%s" % str(uuid.uuid4()).replace("-", "_")
                 params = {
-                    '__tablename__': 'dummy_table_%s' % suffix,
-                    'dummy_id_col': Column(Integer, primary_key=True)
+                    "__tablename__": "dummy_table_%s" % suffix,
+                    "dummy_id_col": Column(Integer, primary_key=True),
                 }
 
-                DummyClass = type('Dummy%s' % suffix, entry.bases, params)
+                DummyClass = type("Dummy%s" % suffix, entry.bases, params)
 
                 base_methods = DummyClass.__dict__.keys()
 
             # Filter mapper methods
             for name, func in entry.methods:
-                if name[0] != '_' and name not in base_methods:
+                if name[0] != "_" and name not in base_methods:
                     if isinstance(func, types.FunctionType):
-                        result_item['methods'].append(name)
+                        result_item["methods"].append(name)
 
         if show_indexes:
-            result_item['indexes'] = get_indexes(entry)
+            result_item["indexes"] = get_indexes(entry)
 
         if show_properties:
-
             # find relationship properties
             for item in entry.properties:
                 if not isinstance(item, ColumnProperty):
-                    result_item['props'].append(item.key)
+                    result_item["props"].append(item.key)
 
             # find column_property() as sql expressions
             for name, item in entry.columns.items():
                 if isinstance(item, Label):
-                    result_item['props'].append(name)
+                    result_item["props"].append(name)
 
         # ordering
-        for key in ('methods', 'props', ):
+        for key in (
+            "methods",
+            "props",
+        ):
             result_item[key].sort()
 
-        for key in ('indexes', ):
-            result_item[key].sort(key=operator.itemgetter('name'))
+        for key in ("indexes",):
+            result_item[key].sort(key=operator.itemgetter("name"))
 
         objects.append(result_item)
 
@@ -257,27 +270,28 @@ def describe(items,
                 for m in entries:
                     try:
                         if str(table) == str(m.table_name):
-                            relations.append({
-                                'from': entry.name,
-                                'by': col.name,
-                                'to': m.name,
-                                'to_col': fk.column.name
-                            })
+                            relations.append(
+                                {
+                                    "from": entry.name,
+                                    "by": col.name,
+                                    "to": m.name,
+                                    "to_col": fk.column.name,
+                                }
+                            )
                     except AttributeError:
                         pass
 
         if entry.inherits:
-
             inh = {
-                'child': entry.name,
-                'parent': EntryItem(mapper=entry.inherits).name,
+                "child": entry.name,
+                "parent": EntryItem(mapper=entry.inherits).name,
             }
 
             inherits.append(inh)
 
             # Delete relation by inherits
             for i, rel in enumerate(relations):
-                if inh['child'] == rel['from'] and inh['parent'] == rel['to']:
+                if inh["child"] == rel["from"] and inh["parent"] == rel["to"]:
                     relations.pop(i)
 
     return objects, relations, inherits
